6dba393c746207aaf0b8df9fb09e6a51b8ac9518
Thomas Opolski <opolskitom@gmail.com>
2019-12-30 17:59:14 -0500

    Official Release

--- Source/webscrape.py
+++ Source/webscrape.py
@@ -2,9 +2,9 @@
 # Date Created: 12/27/19
 # Date Modified: 12/27/19
 
-# Program Description: The idea starts with a basic youtube link parser that gets all
-# videos within a certain search criteria, puts them in a list, and downlaods 
-# them to be processed and analyzed.
+# Program Description: The idea is a basic youtube link parser that gets all
+# videos within a certain search criteria or link, puts them in a list, and downlaods 
+# them to be processed and analyzed. Output websites and codes in the appropriate section.
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,127 +14,180 @@ import cv2
 import numpy as np
 import pyzbar.pyzbar as pyzbar
 from tkinter import *
+from PyQt5 import QtGui, QtWidgets, uic
 
-#global definitions
-tcgCodeQREnabled = True
-websiteQREnabled = True
+class QRCodeScraper:
 
-def getQrDataSet(play):
-    qrDataSet = set()
+    #bools for checkboxes
+    displayVideosEnabled = True
+    websiteQREnabled = True
+    tcgCodeQREnabled = False
+    searchLinkEnabled = False
 
-    cap = cv2.VideoCapture(play.url)
-    while(True):
-        ret,frame = cap.read()
-        if frame is None:
-                break
+    def __init__(self):
+        #initializing window
+        super(QRCodeScraper,self).__init__()
+        self.app = QtWidgets.QApplication([])
+        self.call = uic.loadUi('myWindow.ui')
+        self.call.setWindowTitle("YouTube QR Scraper")
+        self.call.setWindowIcon(QtGui.QIcon("qrcodeicon.png"))
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        #setting functionalities for widgets
 
-        cv2.imshow('videoFramesAnalyzed',frame)
-        #check qr code
-        decodeObj = pyzbar.decode(frame)
+        #buttons (3)
+        self.call.addVideoButton.clicked.connect(self.addVideo)
+        self.call.clearVideosButton.clicked.connect(self.clearVideo)
+        self.call.scanVideosButton.clicked.connect(self.scanVideo)
 
-        if len(decodeObj) > 0:
-            decodedData = decodeObj[0]
-            decodedString = str(decodedData)
+        #check boxes (4)
+        self.call.getWebsitesCheckBox.stateChanged.connect(self.stateChangeWebsite)
+        self.call.getTcgCodesCheckBox.stateChanged.connect(self.stateChangeTCGCode)
+        self.call.displayVideosCheckBox.stateChanged.connect(self.stateChangeVideoDisplay)
+        self.call.searchLinkCheckBox.stateChanged.connect(self.stateChangeSearchLink)
 
-            if len(decodedString) > 20:
+        #display and close window
+        self.call.show()
+        self.app.exec()
 
-                #Website URL Scraping
-                if (websiteQREnabled):
-                    myWebLink = ''
-                    
-                    if decodedString[15:19] == 'http':
-                        i = 15
-                        #read until apostrophe
-                        while (decodedString[i] != "'") :
-                            myWebLink += decodedString[i]
-                            i += 1
+    def addVideo(self):
+        userUrl = self.call.userLinkEdit.text()
+        if (self.searchLinkEnabled):
+            urlListFromSearch = self.getURLSFromSearch(userUrl)
+            for url in urlListFromSearch:
+                self.call.videoQueueList.addItem(url)
+        else:
+            self.call.videoQueueList.addItem(userUrl)
+
+    def clearVideo(self):
+        self.call.videoQueueList.clear()
+
+    def scanVideo(self):
+        outputList = []
+
+        urlLinkList =  [str(self.call.videoQueueList.item(i).text()) \
+            for i in range(self.call.videoQueueList.count())]
+        
+        for urlLink in urlLinkList:
+            outputList += list(self.getFromURL(urlLink))
+
+        for output in outputList:
+            self.call.outputDataList.addItem(output)
+
+    def stateChangeWebsite(self):
+        if self.websiteQREnabled:
+            self.websiteQREnabled = False
+        else:
+            self.websiteQREnabled = True
+
+    def stateChangeTCGCode(self):
+        if self.tcgCodeQREnabled:
+            self.tcgCodeQREnabled = False
+        else:
+            self.tcgCodeQREnabled = True
+
+    def stateChangeVideoDisplay(self):
+        if self.displayVideosEnabled:
+            self.displayVideosEnabled = False
+        else:
+            self.displayVideosEnabled = True
+
+    def stateChangeSearchLink(self):
+        if self.searchLinkEnabled:
+            self.searchLinkEnabled = False
+        else:
+            self.searchLinkEnabled = True
+ 
+    def getQrDataSet(self, play):
+        qrDataSet = set()
+
+        cap = cv2.VideoCapture(play.url)
+        while(True):
+            ret,frame = cap.read()
+            if frame is None:
+                    break
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            #display video if enabled
+            if (self.displayVideosEnabled):
+                cv2.imshow('videoFramesAnalyzed',frame)
+
+            #check qr code
+            decodeObj = pyzbar.decode(frame)
+
+            if len(decodeObj) > 0:
+                decodedData = decodeObj[0]
+                decodedString = str(decodedData)
+
+                if len(decodedString) > 20:
+
+                    #Website URL Scraping
+                    if (self.websiteQREnabled):
+                        myWebLink = ''
                         
-                        qrDataSet.add(myWebLink)
-
-                #PKMN TCG Code Scraping
-                if (tcgCodeQREnabled):
-                    if decodedString[18] == '-' and decodedString[23] == '-' \
-                        and decodedString[27] == '-':
+                        if decodedString[15:19] == 'http':
+                            i = 15
+                            #read until apostrophe
+                            while (decodedString[i] != "'") :
+                                myWebLink += decodedString[i]
+                                i += 1
                             
-                        print(decodedString[15:31])
-                        qrDataSet.add(decodedString[15:31])
-        
-    cap.release()
-    cv2.destroyAllWindows()
+                            qrDataSet.add(myWebLink)
 
-    return qrDataSet
+                    #PKMN TCG Code Scraping
+                    if (self.tcgCodeQREnabled):
+                        if decodedString[18] == '-' and decodedString[23] == '-' \
+                            and decodedString[27] == '-':
+                                
+                            print(decodedString[15:31])
+                            qrDataSet.add(decodedString[15:31])
+            
+        cap.release()
+        cv2.destroyAllWindows()
 
-def getFromURL(myURL):
-    myURL = 'https://www.youtube.com/watch?v=NUSKZMAd-Hs'
+        return qrDataSet
 
-    video = pafy.new(myURL)
+    def getFromURL(self, myURL):
 
-    play = video.getbest(preftype="webm")
-    if (play == None):
-        play = video.getbest(preftype="mp4")
+        try:
+            video = pafy.new(myURL)
 
-    qrDataSet = getQrDataSet(play)
+            play = video.getbest(preftype="webm")
+            if (play == None):
+                play = video.getbest(preftype="mp4")
 
-    return qrDataSet
+            qrDataSet = self.getQrDataSet(play)
 
-def getFromSearch(myURL):
+            return qrDataSet
 
-    #setting up beautiful soup
-    result = requests.get(myURL)
-    src = result.content
-    soup = BeautifulSoup(src, 'html.parser')
+        except:
+            print("Invalid URL")
+            return []
 
-    #list of urls
-    urls = []
+    def getURLSFromSearch(self, myURL):
+        #setting up beautiful soup
+        result = requests.get(myURL)
+        src = result.content
+        soup = BeautifulSoup(src, 'html.parser')
 
-    #finding h3 tag for listed video content
-    for h3_tag in soup.find_all("h3"):
-        a_tag = h3_tag.find('a')
-        if(a_tag):
-            urls.append('https://youtube.com' + a_tag.attrs['href'])
+        #list of urls
+        urls = []
 
-    print(urls)
+        #finding h3 tag for listed video content
+        for h3_tag in soup.find_all("h3"):
+            a_tag = h3_tag.find('a')
+            if(a_tag):
+                urls.append('https://youtube.com' + a_tag.attrs['href'])
 
-    #set shortest to first element
-    shortest = pafy.new(urls[0])
-    print(shortest.length)
-    for url in urls[1:]:
-        video = pafy.new(url)
-        print(video.length)
-        if (video.length < shortest.length and video.length != 0.0):
-            shortest = video
-
-    #for downloading all videos
-    # for url in urls:
-    #     video = pafy.new(url)
-        
-    print("Video to scrape: " + shortest.title)
-    print(shortest.duration)
-
-    play = shortest.getbest(preftype="webm")
-    if (play == None):
-        play = shortest.getbest(preftype="mp4")
-
-    qrDataSet = getQrDataSet(play)
-
-    return qrDataSet
+        return(urls)
 
 
 def main():
 
-    #saved url: https://www.youtube.com/results?search_query=pokemon+card+opening&sp=CAISBAgBEAE%253D
-    #saved myURL: https://www.youtube.com/results?search_query=oogaboogaroogatooga12345&sp=QgIIAQ%253D%253D
-    qrDataSet = getFromSearch("https://www.youtube.com/results?search_query=oogaboogaroogatooga12345&sp=QgIIAQ%253D%253D")
+    #saved search URL: https://www.youtube.com/results?search_query=oogaboogaroogatooga12345&sp=QgIIAQ%253D%253D
 
-    for qrData in qrDataSet:
-            print(qrData)
-
-
-    
-
+    myScraper = QRCodeScraper()
 
 
 if __name__ == "__main__" :
